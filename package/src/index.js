7be55eb0c68ab14477c80ce244ed1feeea510292
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-03-27 23:00:28 +0000

    upd

--- package/src/index.js
+++ package/src/index.js
@@ -1,12 +1,15 @@
 import csvToJson from './csvToJson';
 import parseCsv from './parseCsv';
 
+// uncomment it later
 // i adding utils here only because it's my guess
 // import {
 //   readAllFiles, joinPath, getList
 // } from './utils';
 
-import { mainWrapper, mainWrapper2 } from './mainWrapper';
+// uncomment it later
+// import { mainWrapper, mainWrapper2 } from './mainWrapper';
+
 import findCsvFiles from './findCsvFiles';
 
 export {
@@ -15,8 +18,11 @@ export {
   // parseCsv or we just using it inside of csvToJson
   parseCsv,
   findCsvFiles,
-  mainWrapper, mainWrapper2,
 
+  // uncomment it later
+  // mainWrapper, mainWrapper2,
+
+  // uncomment it later
   // can be removed later
   // readAllFiles,
   // joinPath,
